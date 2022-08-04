c115fd2aafd66b25a30537fe29f93e868df28aa8
jimchen5209 <jimchen5209@gmail.com>
2022-08-04 16:27:33 +0800

    refactor(Express): Invert if in JWT

--- src/middlewares/authJWT.js
+++ src/middlewares/authJWT.js
@@ -2,33 +2,33 @@ import jwt from 'jsonwebtoken';
 
 const verifyToken = (req, res, next) => {
     if (
-        req.headers &&
-        req.headers.auth &&
-        req.headers['auth-method'] === 'JWT'
+        !req.headers ||
+        !req.headers.auth ||
+        req.headers['auth-method'] !== 'JWT'
     ) {
-        // verify token with secrect
-        jwt.verify(
-            req.headers.auth,
-        process.env.JWT_SECRECT,
-            async (err, decode) => {
-                // token invalid
-                if (err) {
-                    return res.status(401).json({
-                        message: 'Invalid JWT token'
-                    });
-                }
-
-                // return user
-                req.user = decode.id;
-                next();
-            }
-        );
-    } else {
         // header invalid
         return res.status(400).json({
             message: 'Invalid header'
         });
     }
+
+    // verify token with secrect
+    jwt.verify(
+        req.headers.auth,
+        process.env.JWT_SECRECT,
+        async (err, decode) => {
+            // token invalid
+            if (err) {
+                return res.status(401).json({
+                    message: 'Invalid JWT token'
+                });
+            }
+
+            // return user
+            req.user = decode.id;
+            next();
+        }
+    );
 };
 
 export { verifyToken };
