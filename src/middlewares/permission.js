61fb7958258ffe41ca81b656a06ad8e81815eb4a
jimchen5209 <jimchen5209@gmail.com>
2022-08-10 17:07:46 +0800

    fix(Express): fix always true

--- src/middlewares/permission.js
+++ src/middlewares/permission.js
@@ -2,7 +2,7 @@ import { getUserFromID } from '../services/user';
 
 const checkAdmin = async (req, res, next) => {
     const user = await getUserFromID(req.user);
-    if (!user.Role !== 1) {
+    if (user.Role !== 1) {
         return res.status(403).json({
             message: 'Permission denied!'
         });
