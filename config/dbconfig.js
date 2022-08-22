ec5b548f4cf32648e1e14ac3356b75484bffa004
jimchen5209 <jimchen5209@gmail.com>
2022-08-22 10:53:02 +0800

    feat(Database): Add stage env and rename default db name

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -4,7 +4,7 @@ module.exports = {
     development: {
         username: process.env.DEV_DB_USERNAME,
         password: process.env.DEV_DB_PASSWORD,
-        database: process.env.DEV_DB_NAME || 'plant_manager',
+        database: process.env.DEV_DB_NAME || 'rental-planter',
         host: process.env.DEV_DB_HOSTNAME || '127.0.0.1',
         port: process.env.DEV_DB_PORT || 3306,
         dialect: 'mysql'
@@ -12,15 +12,23 @@ module.exports = {
     test: {
         username: process.env.CI_DB_USERNAME,
         password: process.env.CI_DB_PASSWORD,
-        database: process.env.CI_DB_NAME || 'plant_manager',
+        database: process.env.CI_DB_NAME || 'rental-planter',
         host: process.env.CI_DB_HOSTNAME || '127.0.0.1',
         port: process.env.CI_DB_PORT || 3306,
+        dialect: 'mysql'
+    },
+    stage: {
+        username: process.env.STAGE_DB_USERNAME,
+        password: process.env.STAGE_DB_PASSWORD,
+        database: process.env.STAGE_DB_NAME || 'rental-planter',
+        host: process.env.STAGE_DB_HOSTNAME || '127.0.0.1',
+        port: process.env.STAGE_DB_PORT || 3306,
         dialect: 'mysql'
     },
     production: {
         username: process.env.PROD_DB_USERNAME,
         password: process.env.PROD_DB_PASSWORD,
-        database: process.env.PROD_DB_NAME || 'plant_manager',
+        database: process.env.PROD_DB_NAME || 'rental-planter',
         host: process.env.PROD_DB_HOSTNAME,
         port: process.env.PROD_DB_PORT,
         dialect: 'mysql'
