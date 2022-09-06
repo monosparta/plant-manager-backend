4cc80bd441d196cfa77a98a0694dad7a9b20a8e0
jimchen5209 <jimchen5209@gmail.com>
2022-09-06 15:46:18 +0800

    test(Test): User Login

---
+++ test/api/user.login.test.js
@@ -0,0 +1,61 @@
+import request from 'supertest';
+import app from '../../src/app';
+
+describe('Test login with user', () => {
+    test('It should block login request when no body is provided.', () => {
+        return request(app)
+            .post('/api/user/login')
+            .expect(400)
+            .then((res) => {
+                expect(res.body.message).toBe('Invalid body');
+            });
+    });
+    test('It should block login request when body is incomplete. (1/2)', () => {
+        return request(app)
+            .post('/api/user/login')
+            .send({ email: 'Lori.Crist@gmail.com' })
+            .expect(400)
+            .then((res) => {
+                expect(res.body.message).toBe('Invalid body');
+            });
+    });
+    test('It should block login request when body is incomplete. (2/2)', () => {
+        return request(app)
+            .post('/api/user/login')
+            .send({ password: 'demo' })
+            .expect(400)
+            .then((res) => {
+                expect(res.body.message).toBe('Invalid body');
+            });
+    });
+
+    test('It should block login request when user is not found.', () => {
+        return request(app)
+            .post('/api/user/login')
+            .send({ email: 'Wiley8@hotmail.com', password: 'demo' })
+            .expect(401)
+            .then((res) => {
+                expect(res.body.message).toBe('Invalid user or password!');
+            });
+    });
+
+    test('It should block login request when password is incorrect.', () => {
+        return request(app)
+            .post('/api/user/login')
+            .send({ email: 'Lori.Crist@gmail.com', password: 'not-demo' })
+            .expect(401)
+            .then((res) => {
+                expect(res.body.message).toBe('Invalid user or password!');
+            });
+    });
+
+    test('It should process the login request.', () => {
+        return request(app)
+            .post('/api/user/login')
+            .send({ email: 'Lori.Crist@gmail.com', password: 'demo' })
+            .expect(200)
+            .then((res) => {
+                expect(res.body.message).toBe('Login success');
+            });
+    });
+});
